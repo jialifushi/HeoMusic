931b3566c48aa4ed0da9c6349098ff4f7cb414d2
jialifushi <101686929+jialifushi@users.noreply.github.com>
2025-07-15 21:51:08 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,108 @@
+[
+  {
+    "title": "SKYLINE - HMHK",
+    "src": "https://m801.music.126.net/20250714211628/a313e7b4a2928ee0f5f4087dfaf67342/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/55482036236/0c0f/2b03/d8ee/82eacd48afdf7a94dc862d4fc3599432.flac"
+  },
+  {
+    "title": "所有心事都放晴 - 苏6",
+    "src": "http://m801.music.126.net/20250714212454/adf5a49b41393f51934a1337ca096da5/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/56260029876/6229/2239/3152/aa821b03cf6d7d4e101795ce44d23752.flac"
+  },
+  {
+    "title": "Bimbo Doll - Dimitri Hander",
+    "src": "http://m701.music.126.net/20250714212600/7511c9e191d654246163d1fcdd433254/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/57173614266/a999/0c70/bb29/ee8208843636121db0b539ffd05865a9.flac"
+  },
+  {
+    "title": "Trust Me - Pandora",
+    "src": "http://m801.music.126.net/20250714212700/cc393c1b34f36be5f7a9ce6636b059b6/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/17285171018/0745/4b19/f2d7/3b7eb5a734da8b6ca09a7889b0ada728.flac"
+  },
+  {
+    "title": "若生命等候（选择你的陈浩南） - YinTianXiang",
+    "src": "http://m801.music.126.net/20250714212756/b082cd46b6fda61fa8c10af55581631a/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/44613231372/7f13/2e1a/465e/8d6acac0bdfe6188798947882fb9a1ae.flac"
+  },
+  {
+    "title": "Pedro - BeMax",
+    "src": "http://m801.music.126.net/20250714212904/ec4fb512af5cabcd267a2ce0f6768803/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/57002869939/df1f/c763/c986/6fb19392e4d3bc5ccc6e300f4e82727c.flac"
+  },
+  {
+    "title": "小猫坏事做尽！ - 小鷹",
+    "src": "http://m801.music.126.net/20250714212954/f5459f55554c6f326328ed6be80342bc/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/56397279814/0ec0/9253/f5c3/d8784f6640a1ab33e94d64789d15ed22.flac"
+  },
+  {
+    "title": "Playboy - David Lyme",
+    "src": "http://m801.music.126.net/20250714213122/d93572941426f5a121b4b4179f704bf8/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/34604047285/b913/ae70/1ba1/ef7e08ce17f959ab34c002ac953247a7.flac"
+  },
+  {
+    "title": "蓝色忧郁＋少女心事＋不羁的风＋Monica - 张国荣",
+    "src": "http://m801.music.126.net/20250714213236/70fff2b5c85e93c33ca91ff785799665/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/5637640408/15ba/dd9c/ac49/ad100b0af409938afbb0a9655bc7a77a.flac"
+  },
+  {
+    "title": "Grind Me Down (Jawster Remix) - Jawster / Lilianna Wilde",
+    "src": "http://m801.music.126.net/20250714213413/b41678497458d2ceb376c719d766e805/jdymusic/obj/w5zDlMODwrDDiGjCn8Ky/1561039751/972b/85c7/e02d/0494971c071cf06c96b2375f1e699db6.flac"
+  },
+  {
+    "title": "Fairytale Montagem - BeMax",
+    "src": "http://m701.music.126.net/20250714213618/4ee57abda53ade164174bf71d360e63e/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/35941669889/ae79/8862/4684/d270bfba515691a9050715a75e3484ab.flac"
+  },
+  {
+    "title": "最后一夜 - 刘美君",
+    "src": "http://m701.music.126.net/20250714213701/444faa1c222ceff3bc3c902e6eb9e3a9/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/60929766153/073f/d265/26b3/793996a558cd732558dbb7677d690877.flac"
+  },
+  {
+    "title": "TWISTED - AViVA",
+    "src": "http://m801.music.126.net/20250714213756/12f97df246061fef897290b5bcc89f58/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/30684339106/3cde/a5a4/eb21/a4ef0dff5cc4df912ba2880ddef05b60.flac"
+  },
+  {
+    "title": "E: ku - ASTER / JERIDE",
+    "src": "http://m801.music.126.net/20250714213918/e54bb3b984897bb671ddd31e635fd7c4/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/17028169442/ebff/da3e/4d4a/1f0045e6566d3016c89dccf44ffdbe14.flac"
+  },
+  {
+    "title": "悬溺 - 葛东琪",
+    "src": "http://m801.music.126.net/20250714214135/f8888fbcce1704d3647917e02f155a5f/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/28558885187/791c/f439/24be/9150994d6473cbed2a73f19d1dd19f92.flac"
+  },
+  {
+    "title": "凄美地 - 郭顶",
+    "src": "http://m801.music.126.net/20250714214222/853015b98d7967441109f3eafdbbb6c6/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/28481676662/90b2/5b9e/3b20/10d8aaaf1182419ed8bc9160f806dd21.flac"
+  },
+  {
+    "title": "Windy Hill - 羽肿",
+    "src": "http://m701.music.126.net/20250714214322/9937f7ec2ecdc252dc1e4178abba577d/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/28481691835/445e/e5fb/3fa1/3c9bfcff10904e2a822ab95a8a7d26ef.flac"
+  },
+  {
+    "title": "Better Fool - Vanished",
+    "src": "http://m701.music.126.net/20250714214514/ebc7a3e18aa1e4d429f3b5b951156838/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/28798312191/3b21/816d/d5fd/5115936fd2d4c29e5c5c22dd2c2390c5.flac"
+  },
+  {
+    "title": "Radio - Sigala / MNEK",
+    "src": "http://m701.music.126.net/20250714214559/7301e8751a93e39931555f1c3a1a84e1/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/32069275220/adb6/5b69/33f6/df9af1b84f5142378bdca40cd4642c8f.flac"
+  },
+  {
+    "title": "Reborn - 玉置成実",
+    "src": "http://m701.music.126.net/20250714214642/fa5c6d97cfb8e57ee67cd5c4fa109817/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/58665868492/ab05/e465/1263/b3686e1277e24bbc12e7bb2396bb62ee.flac"
+  },
+  {
+    "title": "Plastic Dog (Radio Edit) - YahikoNy",
+    "src": "http://m801.music.126.net/20250714214718/ebcd054443e2598c821faccbad123d0e/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/31351225974/8386/b9ab/54d2/c30728e67220637abb85490844826d84.flac"
+  },
+  {
+    "title": "毒药 (释怀DJ版) - SN",
+    "src": "http://m701.music.126.net/20250714214757/ab7122bce79d651231c0d53889e0e24a/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/30094470954/98d4/f96f/831e/50430f630cd5be817f04fef7d95932d2.flac"
+  },
+  {
+    "title": "China-Hometown - Latifa",
+    "src": "http://m701.music.126.net/20250714214843/74493d5754d69fde491fbc2293d8c2db/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/29228465145/c37b/bbf2/d552/9d90b80b9b1c20bfda6580cddb3162e6.flac"
+  },
+  {
+    "title": "Wind Letter - Jamvana / VZEUS",
+    "src": "http://m801.music.126.net/20250714212329/32927af6b8b47aed09b6b6859da3c7ed/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/46037675485/f126/38e7/4a8c/563629cd6e758cc68f2bddda5eeb3e5f.flac"
+  }
+]
+
+
+
+
+
+
+
+
+
+
